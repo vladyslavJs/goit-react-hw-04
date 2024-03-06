8058ee1d6f725e81891a68396d3fbf6337516e51
vladyslavJs <vladyslavmelnyk15@gmail.com>
2024-03-06 19:20:02 +0100

    update-App

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,48 +1,59 @@
+import { useEffect, useState } from "react";
 import { fetchImages } from "../../gallery-api";
+import css from "./App.module.css";
+
 import SearchBar from "../SearchBar/SearchBar";
 import ImageGallery from "../ImageGallery/ImageGallery"
-import ImageCard from "../ImageCard/ImageCard";
 import ButtonLoadMore from "../ButtonLoadMore/ButtonLoadMore";
-import ErrorMessage from "./ErrorMessage/ErrorMessage";
+import ErrorMessage from "../ErrorMessage/ErrorMessage";
+import Loader from "../Loader/Loader";
+import ImageModal  from "../ImageModal/ImageModal";
+import toast from 'react-hot-toast';
 
-import toast, { Toaster } from 'react-hot-toast';
-import { useEffect, useState } from "react";
+import { Toaster } from "react-hot-toast";
 
 
 
 function App() {
+  const [searchQuery, setSearchQuery] = useState("");
   const [images, setImages] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
   const [error, setError] = useState(false);
   const [page, setPage] = useState(1);
-  const [searchQuery, setSearchQuery] = useState("");
   const [showBtn, setShowBtn] = useState(false);
 
+  const [isOpen, setIsOpen] = useState(false);
+  const [content, setContent] = useState({});
+
   useEffect(() => {
-  // HTTP запит не потрібно робити коли порожній імпут, тому робимо таку логіку. В результаті ми перевіряємо якийсь вираз   і якщо це try, то код знизу просто не виконується!
     if (searchQuery === '') {
       return;
     }
 
     const getImages = async () => {
       try {
-        setIsLoading(true);
-        // setError(false);
+        // setIsLoading(true);
+        setError(false);
         const { imageData, totalPages } = await fetchImages(searchQuery, page);
         console.log(fetchImages);
         
         setImages((prevImages) => {
           return [...prevImages, ...imageData];
         }); //операція від попереднього, без перезаписування
-        
-        // toast.success('Successfully!')
-
+        toast.success('Successfully!',{
+          style: {
+                    fontSize: '10px',
+                    background: '#fff',
+                    color: '#808080',
+                    padding: '6px',
+                    boxShadow: '0 4px 8px rgba(0, 0, 0, 0.1)',
+                    borderRadius: '4px',
+                  },
+                }); 
         setShowBtn(totalPages !== page && imageData.length > 0);
 
       } catch (error) {
-        console.log(error);
         setError(true);
-
       }
       finally {
         setIsLoading(false);
@@ -54,21 +65,41 @@ function App() {
   const handleSearch = (newQuery) => {
     setSearchQuery(newQuery);
     setPage(1);
-    setImages([]); //так ми скидаємо масив даних, щоб почати пошук з порожньої інформації
+    setImages([]);
+    setError(false);
+    setShowBtn(false);
   };
 
   const handleLoadMore = () => {
     setPage(page + 1);
-    
-  };  
+    setIsLoading(true);
+  };
+
+  const handleOpen = (value) => {
+    setIsOpen(true);
+    setContent(value);
+  };
+
+  const handleClose = () => {
+    setIsOpen(false);
+  };
+  
   return (
-    <div>
+    <div className={css.main}>
       < SearchBar onSubmit={handleSearch} />
-      {images.length > 0 && <ImageGallery gallery={images} />}
+      {images.length > 0 && (
+          <ImageGallery gallery={images} onOpen={handleOpen} />
+        )}
+      {isLoading && <Loader/>}
       {error && <ErrorMessage  />}
-      {showBtn && <ButtonLoadMore onClick={handleLoadMore}/>}
- 
+      {showBtn && <ButtonLoadMore onClick={handleLoadMore} />}
+
+      <ImageModal isOpen={isOpen} onClose={handleClose} content={content} />
       
+      <Toaster
+        position="top-left"
+      />
+ 
     </div>
   )
 }
