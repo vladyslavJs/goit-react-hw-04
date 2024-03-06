5789d9cb78da8c9849b03293926f7c4f8ac51fe9
vladyslavJs <vladyslavmelnyk15@gmail.com>
2024-03-06 19:20:46 +0100

    Update-ImageCard

--- src/components/ImageCard/ImageCard.jsx
+++ src/components/ImageCard/ImageCard.jsx
@@ -2,7 +2,6 @@ import css from './ImageCard.module.css'
 import { FcLike } from "react-icons/fc";
 import { FcBusinessman } from "react-icons/fc";
 import { LuDownload } from "react-icons/lu";
-import { CiInstagram } from "react-icons/ci";
 
 
 export const ImageCard = ({
@@ -12,7 +11,7 @@ export const ImageCard = ({
     likes,
     description,
     urls: { small },
-    user: { last_name, instagram_username },
+    user: { last_name },
     links: { download },
   },
 }) => {
@@ -28,30 +27,15 @@ export const ImageCard = ({
         onClick={() => onOpen(imageCard)}
         className={css.image}
       />
-      <div >
         <ul className={css.galleryStatistic}>
-          <li>
+          <li className={css.li}>
             <p className={css.statistic}> <FcLike /><span>{likes}</span></p>
           </li>
           <li>
             <p className={css.statistic}> <FcBusinessman /><span>{last_name}</span></p>
           </li>
-          <li>
-            <p className={css.statistic}> <CiInstagram /><span className={css.span}>{instagram_username}</span></p>
-          </li>
-          <button onClick={handleDownloadClick} className={css.button}><LuDownload/></button>
+          <button onClick={handleDownloadClick} className={css.button}>Download <LuDownload/></button>
         </ul>
-        
-        {/* <div >
-          <p>
-            Likes: <span>{likes}</span>
-          </p>
-          <p>
-            Author: <span>{last_name}</span>
-          </p>    
-        </div> */}
-        {/* <button onClick={handleDownloadClick}>Download</button> */}
-      </div>
     </div>
   );
 };
