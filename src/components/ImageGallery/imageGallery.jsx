3a6966cf4285dfb73e42d27ac0b79763ef0b321c
vladyslavJs <vladyslavmelnyk15@gmail.com>
2024-03-06 19:21:05 +0100

    update-ImageGallery

--- src/components/ImageGallery/imageGallery.jsx
+++ src/components/ImageGallery/imageGallery.jsx
@@ -1,12 +1,12 @@
 import { ImageCard } from "../ImageCard/ImageCard";
 import css from './ImegaGallery.module.css'
 
-export default function ImageGallery({ gallery }) {
+export default function ImageGallery({ gallery, onOpen}) {
   return (
     <ul className={css.containerGallery}>
       {gallery.map((item) => (
         <li key={item.id} className={css.galleryItems}>
-          <ImageCard imageCard={item}/>
+          <ImageCard imageCard={item} onOpen={onOpen}/>
         </li>
       ))}
     </ul>
