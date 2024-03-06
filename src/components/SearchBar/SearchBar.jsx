a1e092a504a70fe317774d651e591c9f60e22a5a
vladyslavJs <vladyslavmelnyk15@gmail.com>
2024-03-06 19:21:42 +0100

    update-searchBar

--- src/components/SearchBar/SearchBar.jsx
+++ src/components/SearchBar/SearchBar.jsx
@@ -1,17 +1,31 @@
 import { Formik, Form, Field } from "formik";
+import toast from 'react-hot-toast';
 
 import { CiSearch } from "react-icons/ci";
 import css from './SearchBar.module.css'
+import { BiLogoMailchimp } from "react-icons/bi";
 
 export default function SearchBar({ onSubmit }) {
+  
     return (
-      <header className={css.header}>
+      <header className={css.header}> 
         <Formik initialValues={{ searchQuery: '' }}
             onSubmit={(values, actions) => {
-                onSubmit(values.searchQuery);
+              onSubmit(values.searchQuery);
+              if (!values.searchQuery) {
+                toast.error('Text input is required for image search.', {
+                  style: {
+                    background: '#fff',
+                    color: '#808080',
+                    padding: '6px',
+                    boxShadow: '0 4px 8px rgba(0, 0, 0, 0.1)',
+                    borderRadius: '8px',
+                  },
+                });
+              }
                 actions.resetForm();
         }}>
-        <Form className={css.form}>
+          <Form className={css.form}><BiLogoMailchimp className={css.iconHeader} />
             <Field
             type="text"
             name="searchQuery" 
