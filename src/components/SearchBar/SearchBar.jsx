4ca1aaaacc5dc7826142718fb4425b4d747df431
vladyslavJs <vladyslavmelnyk15@gmail.com>
2024-03-08 11:16:20 +0100

    Fix-problem-toast

--- src/components/SearchBar/SearchBar.jsx
+++ src/components/SearchBar/SearchBar.jsx
@@ -6,38 +6,37 @@ import css from './SearchBar.module.css'
 import { BiLogoMailchimp } from "react-icons/bi";
 
 export const SearchBar = ({ onSubmit }) => {
-  
-    return (
-      <header className={css.header}> 
-        <Formik initialValues={{ searchQuery: '' }}
-            onSubmit={(values, actions) => {
-              onSubmit(values.searchQuery);
-              if (values.searchQuery.trim() === '') {
-                toast.error('Text input is required for image search.', {
-                  style: {
-                    background: '#fff',
-                    color: '#808080',
-                    padding: '6px',
-                    boxShadow: '0 4px 8px rgba(0, 0, 0, 0.1)',
-                    borderRadius: '8px',
-                  },
-                });
-              }
-              actions.resetForm();
+  return (
+    <header className={css.header}>
+      <Formik initialValues={{ searchQuery: '' }}
+        onSubmit={(values, actions) => {
+          if (values.searchQuery.trim() === '') {
+            return toast.error('Text input is required for image search.', {
+              style: {
+                background: '#fff',
+                color: '#808080',
+                padding: '6px',
+                boxShadow: '0 4px 8px rgba(0, 0, 0, 0.1)',
+                borderRadius: '8px',
+              },
+            });
+          }
+          onSubmit(values.searchQuery);
+          actions.resetForm();
         }}>
-          <Form className={css.form}><BiLogoMailchimp className={css.iconHeader} />
-            <Field
+        <Form className={css.form}><BiLogoMailchimp className={css.iconHeader} />
+          <Field
             type="text"
-            name="searchQuery" 
+            name="searchQuery"
             autoComplete="off"
             autoFocus
             placeholder="Search images and photos"
             className={css.field}
-            
-            ></Field>
-            <button className={css.button} type="submit"><CiSearch className={css.icon} /></button>
+          
+          ></Field>
+          <button className={css.button} type="submit"><CiSearch className={css.icon} /></button>
         </Form>
-        </Formik>
+      </Formik>
     </header>
   );
 }
