718d41cfe068de1eedad725d13dc46d2024f7c4b
vladyslavJs <vladyslavmelnyk15@gmail.com>
2024-03-10 00:05:56 +0100

    comm

--- src/components/SearchBar/SearchBar.jsx
+++ src/components/SearchBar/SearchBar.jsx
@@ -23,8 +23,7 @@ export const SearchBar = ({ onSubmit }) => {
           }
           onSubmit(values.searchQuery);
           actions.resetForm();
-        }}
-      >
+        }}>
         <Form className={css.form}><BiLogoMailchimp className={css.iconHeader} />
           <Field
             type="text"
